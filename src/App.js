b97c9b16e4f3da1224849313d922de25f412e100
Aarthipriya A <a.aarthipriya2021@gmail.com>
2023-12-19 16:13:51 +0530

    added starter files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Product from "./components/Product";
+import Review from "./components/Review";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <main>
+        <div id="home">
+          <Home />
+        </div>
+        <div id="menu">
+          <Menu />
+        </div>
+        <div id="about">
+          <About />
+        </div>
+        <div id="products">
+          <Product />
+        </div>
+        <div id="review">
+          <Review />
+        </div>
+      </main>
+
+      <Footer />
     </div>
   );
 }
